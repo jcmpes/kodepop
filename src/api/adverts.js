ceb15464cf55a6241ba01ad40ccb83eeb3ee0b1b
jc <jcmpes@gmail.com>
2021-03-26 13:07:32 +0100

    feat: Added image and title to AdsPage

--- src/api/adverts.js
+++ src/api/adverts.js
@@ -1,6 +1,6 @@
-import client from './client'
+import client from './client';
 
-const allAds = [
+export const getAds = async() => ([
     {
         "id": "3dad6450-0e61-4363-a2c7-2b669fe86508",
         "createdAt": "2021-03-26T05:22:30.000Z",
@@ -12,13 +12,4 @@ const allAds = [
         ],
         "photo": "http://localhost:3001/public/1616736150445-754816386.png"
     }
-]
-
-export const getAds = () => {
-    // const url = 'api/v1/adverts/';
-    // const response = await client.get(url);
-    // const data = response.data;
-    
- 
-    return allAds;
-};
+])
--- src/components/ads/AdsPage.js
+++ src/components/ads/AdsPage.js
@@ -1,12 +1,29 @@
-import { getAds } from '../../api/adverts'
+import React from 'react';
+import { getAds } from '../../api/adverts';
 
-const items = getAds()
+let allAds = []
+getAds().then((response) => allAds = response)
 
+// Component to load ads
 const AdsPage = () => {
+    const [ads, setAds] = React.useState(allAds)
+    ads.map(item => (
+        <li key={item.id} >
+            <div style={{ display: 'flex' }}>
+                <img src={item.photo} style={{ flex: '0 0 auto' }}/>
+                <h2 style={{ flex: '1' }}>{item.name}</h2>
+            </div>
+        </li>
+    ));
+
+    React.useEffect(() => {
+        getAds().then(response => setAds(response))
+    }, [])
+
     return(
         <div className="adsPage">
             <ul>
-                {items.map(ad => (
+                {ads.map(ad => (
                     <li key={ad.id}>
                         <img src={process.env.REACT_APP_API_BASE_URL, ad.photo} />
                         {ad.name}
