f126151e855e736d07c199d738297096a8755f2a
jc <jcmpes@gmail.com>
2021-03-27 09:40:52 +0100

    chores: Improved styles in AdsPlage

--- src/components/ads/AdsPage.js
+++ src/components/ads/AdsPage.js
@@ -7,28 +7,24 @@ getAds().then((response) => allAds = response)
 // Component to load ads
 const AdsPage = () => {
     const [ads, setAds] = React.useState(allAds)
-    ads.map(item => (
-        <li key={item.id} >
-            <div style={{ display: 'flex' }}>
-                <img src={item.photo} style={{ flex: '0 0 auto' }}/>
-                <h2 style={{ flex: '1' }}>{item.name}</h2>
-            </div>
-        </li>
-    ));
 
     React.useEffect(() => {
         getAds().then(response => setAds(response))
     }, [])
 
+    const items = ads.map(item => (
+        <li key={item.id} style={{ listStyle: 'none' }}>
+            <div style={{ display: 'flex' }}>
+                <img src={item.photo} style={{ flex: '0 0 auto', maxWidth: '300px' }}/>
+                <h2 style={{ flex: '1', textAlign: 'left', marginLeft: '1rem' }}>{item.name}</h2>
+            </div>
+        </li>
+    ));
+
     return(
         <div className="adsPage">
             <ul>
-                {ads.map(ad => (
-                    <li key={ad.id}>
-                        <img src={process.env.REACT_APP_API_BASE_URL, ad.photo} />
-                        {ad.name}
-                    </li>
-                ))}
+                {items}
             </ul>
         </div>
     )
