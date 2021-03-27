81c970c59dc84b1e4eb21ed0f4720bca5f4dd415
jc <jcmpes@gmail.com>
2021-03-27 13:04:57 +0100

    feat: Added AdsPage.css

--- src/components/ads/AdsPage.js
+++ src/components/ads/AdsPage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { getAds } from '../../api/adverts';
+import './AdsPage.css'
 
 let allAds = []
 getAds().then((response) => allAds = response)
@@ -9,14 +10,14 @@ const AdsPage = () => {
     const [ads, setAds] = React.useState(allAds)
 
     React.useEffect(() => {
-        getAds().then(response => setAds(response))
+        getAds().then(setAds)
     }, [])
 
     const items = ads.map(item => (
-        <li key={item.id} style={{ listStyle: 'none' }}>
-            <div style={{ display: 'flex' }}>
-                <img src={item.photo} style={{ flex: '0 0 auto', maxWidth: '300px' }}/>
-                <h2 style={{ flex: '1', textAlign: 'left', marginLeft: '1rem' }}>{item.name}</h2>
+        <li key={item.id} onClick={e => alert("Contruyendo enlace")}>
+            <div className='ad-wrapper'>
+                <img src={item.photo} />
+                <h2>{item.name}</h2>
             </div>
         </li>
     ));
